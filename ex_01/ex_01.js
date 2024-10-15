57c7dbe91cecb9250e14e7f72f97271f478ad5ef
Rajeeva8369 <rajeeva.ravichandiran@epitech.digital>
2024-10-15 14:28:01 +0200

    ex_01.js

--- ex_01/ex_01.js
+++ ex_01/ex_01.js
@@ -1,7 +1,7 @@
 function display_weather_chart(longitude, latitude) {
-    var affiche = `https://www.7timer.info/bin/civil.php`;
-    var img = document.createElement('img');
-    img.src = affiche;
+    const img = document.createElement('img');
+    img.src = `https://www.7timer.info/bin/civil.php?lon=${longitude}&lat=${latitude}`;
     img.alt = 'Weather Forecast Chart';
-    document.body.appendChild(img);}
+    document.body.appendChild(img);
+}
 display_weather_chart(113.17, 23.09);
